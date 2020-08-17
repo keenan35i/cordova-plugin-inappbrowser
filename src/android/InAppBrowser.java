0689dea7e39fd60c8eeb01d2c2694696a8b784fb
keenan35i <zane1111@icloud.com>
2020-08-17 12:48:21 -0500

    Update InAppBrowser.java

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -961,7 +961,6 @@ public class InAppBrowser extends CordovaPlugin {
                 settings.setJavaScriptEnabled(true);
                 settings.setJavaScriptCanOpenWindowsAutomatically(true);
                 settings.setPluginState(android.webkit.WebSettings.PluginState.ON);
-                settings.setRenderPriority(settings.RenderPriority.HIGH);
                 settings.setUseWideViewPort(true);
                 settings.setSupportZoom(true);
                 settings.setBuiltInZoomControls(false);
